a2c50554053a6435906052403e9a572e8a423ba6
nishchay213 <patilnishchay23.com>
2025-01-15 15:04:48 +0530

    minor changes

--- compute_unit.c
+++ compute_unit.c
@@ -1,4 +1,15 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "primary_storage.h"
 
+int getValue(short row, short col)
+{
+    Cell *cell = getCell(row, col);
+    if (cell->state == 0)
+    {
+        return cell->value;
+    }
+    else
+    {
+    }
+}
--- compute_unit.h
+++ compute_unit.h
@@ -3,10 +3,10 @@
 
 #include "primary_storage.h"
 
-void valueExpression(short row, short col, short type, int value, short refcell_row, short refcell_col);
-void arithmeticExpression(short row, short col, short operation, short cell1_row, short cell1_col, short cell2_row, short cell2_col);
-void functionExpression(short topleft_row, short topleft_col, short bottomright_row, short bottomright_col, short type);
-
+void setValueExpression(short row, short col, short type, int value, short refcell_row, short refcell_col);
+void setArithmeticExpression(short row, short col, short operation, short cell1_row, short cell1_col, short cell2_row, short cell2_col);
+void setFunctionExpression(short topleft_row, short topleft_col, short bottomright_row, short bottomright_col, short type);
+int getValue(short row, short col);
 
 
 
