376f3c48bb7d00123c3717a7c89a293cd73052aa
nishchay213 <patilnishchay23.com>
2025-02-19 22:55:27 +0530

    fixed some more

--- parsing/cell_indexing.h
+++ parsing/cell_indexing.h
@@ -2,7 +2,7 @@
 #define CELL_INDEXING_H
 #include "../backend/primary_storage.h"
 #include <stdbool.h>
-bool cellWithinExpression(const Expression* expression, short row, short col);
+bool cellWithinExpression(const Expression expression, short row, short col);
 short col_label_to_index(const char *col);
 void col_index_to_label(short col, char *buffer);
 int parse_cell_reference(const char *ref, short *row, short *col);
